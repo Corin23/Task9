493f22694aa82a1c51803e305965a768ab2076a3
Corin23 <dobricacorin5@yahoo.com>
2022-09-05 16:24:38 +0300

    changed malloc with calloc

--- txtproc.c
+++ txtproc.c
@@ -28,7 +28,7 @@ static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb,
   return realsize;
 }
 
-int *readURL(const char *URL) {
+static int *readURL(const char *URL) {
   CURL *curl_handle;
   CURLcode res;
   int c;
